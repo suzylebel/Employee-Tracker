1dcfe4812849f2a72b214f288fda8804e562c668
Suzy Le Bel <suzy.lebel@gmail.com>
2020-11-30 14:39:05 -0800

    push for tutoring

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,8 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
-// const table = require("console.table");
+const table = require("console.table");
 
-// seeder files
+// seeder files (?)
 
 // create db connection 
 var connection = mysql.createConnection({
@@ -16,16 +16,37 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "idateQuinn5",
-  database: "top_songsDB"
+  database: "employee_trackerDB"
 });
 
 // make the connection
 connection.connect(function (err) {
   if (err) throw err;
+  // afterConnection();
+  console.log(" ▄▄▄▄▄▄▄▄▄▄▄  ▄▄       ▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄            ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄       ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄    ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄");
+console.log("▐░░░░░░░░░░░▌▐░░▌     ▐░░▌▐░░░░░░░░░░░▌▐░▌          ▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌");
+console.log("▐░█▀▀▀▀▀▀▀▀▀ ▐░▌░▌   ▐░▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░█▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀       ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌ ▐░▌ ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌");
+console.log('▐░▌          ▐░▌▐░▌ ▐░▌▐░▌▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌                    ▐░▌     ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌▐░▌  ▐░▌          ▐░▌       ▐░▌');
+console.log('▐░█▄▄▄▄▄▄▄▄▄ ▐░▌ ▐░▐░▌ ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌          ▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄           ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌          ▐░▌░▌   ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌');
+console.log('▐░░░░░░░░░░░▌▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌▐░▌          ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌          ▐░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌          ▐░░▌    ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌');
+console.log('▐░█▀▀▀▀▀▀▀▀▀ ▐░▌   ▀   ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌          ▐░▌       ▐░▌ ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀           ▐░▌     ▐░█▀▀▀▀█░█▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░▌          ▐░▌░▌   ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀█░█▀▀ ');
+console.log('▐░▌          ▐░▌       ▐░▌▐░▌          ▐░▌          ▐░▌       ▐░▌     ▐░▌     ▐░▌          ▐░▌                    ▐░▌     ▐░▌     ▐░▌  ▐░▌       ▐░▌▐░▌          ▐░▌▐░▌  ▐░▌          ▐░▌     ▐░▌  ');
+console.log('▐░█▄▄▄▄▄▄▄▄▄ ▐░▌       ▐░▌▐░▌          ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌     ▐░▌     ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄           ▐░▌     ▐░▌      ▐░▌ ▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌ ▐░▌ ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌      ▐░▌ ');
+console.log('▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌          ▐░▌     ▐░▌       ▐░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌');
+console.log(' ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀            ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀       ▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀            ▀       ▀         ▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀    ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀ ');
+//  console.log(" _____                 _                         _____               _\n| ____|_ __ ___  _ __ | | ___  _   _  ___  ___  |_   _| __ __ _  ___| | _____ _ __\n |  _| | '_ ` _ \| '_ \| |/ _ \| | | |/ _ \/ _ \   | || '__/ _` |/ __| |/ / _ \ '__|\n| |___| | | | | | |_) | | (_) | |_| |  __/  __/   | || | | (_| | (__|   <  __/ |\n|_____|_| |_| |_| .__/|_|\___/ \__, |\___|\___|   |_||_|  \__,_|\___|_|\_\___|_|\n|_|\n|___/")
   start();
 });
-
-
+// --------------connection query after function-----------
+// function afterConnection() {
+//   connection.query("SELECT * FROM newEmployee", function(err, res) {
+//     if (err) throw err;
+//     console.log(res);
+//     connection.end();
+//   });
+ 
+// }
+// ----------------------- PROMPT USER--------------
 function start() {
   inquirer
   .prompt({
@@ -33,7 +54,7 @@ function start() {
       name: "action",
       message: "What would you like to do?",
       choices: [
-        "Add an a role",
+        "Add a a role",
         "Add an Employee",
         "Add a Department",
         "View all roles",
@@ -42,8 +63,9 @@ function start() {
         "exit"
       ]
     }).then((answers) => {
+      // ----------------------SWTICH STATEMENT-----------------
     switch (answers.action) {
-      case "Add an a role": 
+      case "Add a a role": 
         addRole();
         break;
       
@@ -66,18 +88,60 @@ function start() {
       case "View all Departments": 
         viewDepartments();
         break;
+
+        case "View all roles": 
+        viewRoles();
+        break;
       
-      case "exit": 
-        connection.end
-        console.log("See your team")
+      case "Delete Employees": 
+        deleteEmployees();
+        break;
+      
+      // case "View all Departments": 
+      //   viewDepartments();
+      //   break;  
+      
+      case "See the entire team": 
+      
+        viewEntireTeam();
         break;
     }
   });
   }
 
+  function deleteEmployees() {
+    connection.query('SELECT * FROM newEmployee', function(err, res) {
+      if (err) throw err;
+    inquirer
+    .prompt([
+      {
+      name: "delete",
+      type: "rawlist",
+      message: "What employee do you want to delete?",
+      choices: function () {
+        let choiceEmmployeeDel = [];
+        for (var i = 0; i < res.length; i++) {
+          choiceEmmployeeDel.push(res[i].fname + ' ' + res[i].lname);
+        }
+        return choiceEmmployeeDel
+      },
+    }
+  ]).then(function (answer) {
+    let employee = answer.choices.split('')
+    connection.query("DELETE FROM newEmployee where fname = ? AND lname = ?")
+    console.log("Employee was successfully removed!")
+    viewEmployees();
+    start();
+  // helper function "get all employees" get them by index #
+
+// by index (bob, joe) by id. Query by ID
+  },
+
+  // ==================ADD EMPLOYEE========================
     function addEmployee() {
       inquirer
-        .prompt({
+        .prompt([
+          {
           name: "fname",
           type: "input",
           message: "What is the first name of the employee?"
@@ -91,41 +155,125 @@ function start() {
             name: "employeerole",
             type: "list",
             message: "What is the role of the employee?",
-            choices: ["MANAGER", "ASSOCIATE", "INTERN", "SOFTWARE DEV"]
+            choices: [1, 2, 3, 4]
           },
           {
             name: "manager",
             type: "input",
-            message: "Who is the employee's manager?",
-          }
-        )
-        .then(function (answer) {
-          connection.query(
-            "INSERT INTO newEmployee SET ? ",
-            {
+            message: "Who is the employee's manager?"
+          },
 
+        ]).then(function (answer) {
+          console.log("Employee Added!");
+        query = "INSERT INTO newEmployee SET ? ";
+            connection.query(query,{
               fname: answer.fname,
               lname: answer.lname,
               role_id: answer.employeerole,
-              manager: answer.manager
-
+              manager: answer.manager,
+            })
+            console.table(answer)
+        
+            viewEmployees();
+            start();
+            
             },
-            function(err) {
+            function(err, res) {
               if (err) throw err;
-              console.log("employee added successfully!");
-              start();
+
             }
           );
-        });
+        
+    })
+
+  //   function updateEmployee(){
+  //     console.log("updating...\n");
+  //     connection.query("Update newEmployee SET ? WHERE ?"),
+  //     [{
+
+  //     },
+  //     {
+  //         price: ""
+  //     }
+  // ],
+  // function (err, res) {
+  //   if (err) throw err;
+  //   console.log(res.affectedRows + " employee updated!\n");
+    
+// ----------------------------VIEW EMPLOYEE ------------------------
+    function viewEmployees() {
+      console.log("Showing all current employees...\n");
+      connection.query("SELECT * FROM newEmployee", function(err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.table(res);
+        start();
+        // connection.end();
+      });
+    }
+    
+    // ----------------------------VIEW Deparment ------------------------
+    function viewDepartments() {
+      console.log("Showing all Departments...\n");
+      connection.query("SELECT * FROM Department", function(err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.table(res);
+        start();
+        // connection.end();
+      });
+    })
+
+    // / ==================ADD Role========================
+    function addRole() {
+      inquirer
+        .prompt([
+          {
+          name: "title",
+          type: "input",
+          message: "What Role would you like to add?"
+        },
+        {
+          name: "salary",
+          type: "list",
+          message: "What is the salary for this role?",
+          choices: [20000, 50000, 60000, 70000, 80000, 90000, 100000, 120000]
+        },
+
+        ]).then(function (answer) {
+          console.log("Employee Added!");
+        query = "INSERT INTO employeeRole SET ? ";
+            connection.query(query,{
+             title: answer.title,
+              salary: answer.salary,
+             
+            })
+            console.table(answer)
+        
+            viewRoles();
+            start();
+            
+            },
+            function(err, res) {
+              if (err) throw err;
+
+            }
+          );
+        
     }
 
+    // ----------------------------VIEW Employee Roles ------------------------
+    function viewRoles() {
+      console.log("Showing all Roles...\n");
+      connection.query("SELECT * FROM employeeRole", function(err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.table(res);
+        start();
+        // connection.end();
+      });
+    }
 
-    function viewEmployees() {
-      inquirer
-    .prompt({
-      name: "viewemployee",
-      type: "list",
-      message: "What employee would you like to see?"
-    })
   }
 
+  
--- Develop/schema.sql
+++ Develop/schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE newEmployee (
   id INT NOT NULL AUTO_INCREMENT,
   fname VARCHAR(30) NOT NULL,
   lname VARCHAR(30) NOT NULL,
-   role_id INT NULL,
+  role_id INT NULL,
   manager VARCHAR(100) NULL,
   PRIMARY KEY (id)
 )
@@ -32,11 +32,11 @@ VALUES ("Marketing"), ("Finance"), ("HR"), ("IT Dept")
 SELECT * FROM Department;
 
 INSERT INTO employeeRole (title, salary, department_id)
-VALUES ("Marketing Manager", "70000", "")
+VALUES ("Marketing Manager", "70000", 3)
 SELECT * FROM employeeRole;
 
 INSERT INTO newEmployee (fname, lname, role_id, manager)
-VALUES ("Suzy", "Le BeL", "Marketing Manager", "Ben TA"), ("Colbi", "Le Colburn", "Intern", "Suzy Le Bel")
+VALUES ("Suzy", "Le BeL", 3, "Ben TA"), ("Colbi", "Le Colburn", 2, "Suzy Le Bel")
 
 SELECT * FROM newEmployee;
 
